04f700ee76f03badadfbe4dc4631a1f4a5abc46c
justschen <justchen@microsoft.com>
2026-02-18 10:59:54 -0800

    fix some compilation errors'

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,8 @@
+declare module '*.css';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg';
 declare module '*.gif';
+
+declare function require(path: string): string;
